4dad6f9b5b0056f8254dc78d53bd0dc8dbbea4a5
mframpton21 <mike521@gmail.com>
2015-01-17 14:17:46 -0700

    Submitting problems 10 and 11

--- practice.js
+++ practice.js
@@ -249,16 +249,22 @@ var colt = {
     spiritAnimal: 'Young Male Horse'
 }
 
+  //Code Here
+devMountainEmployees.push(tyler);
+devMountainEmployees.push(cahlan);
+devMountainEmployees.push(ryan);
+devMountainEmployees.push(colt);
 
+// Now let's say Cahlan has a mental breakdown and has to take a leave of 
+// absense to 'find himself'. Loop through your devMountainEmployees until 
+// you find cahlan, then remove him from the array.
 
   //Code Here
-
-// Now let's say Cahlan has a mental breakdown and has to take a leave of absense to 'find himself'.
-// Loop through your devMountainEmployees until you find cahlan, then remove him from the array.
-
-  //Code Here
-
-
+for (var i = 0; i < devMountainEmployees.length; i++) {
+  if (devMountainEmployees[i].name === "Cahlan") {
+    devMountainEmployees.splice(i, 1);
+  }
+}
 
 
 // PROBLEM 11
@@ -286,14 +292,13 @@ var data = [
     }
 ];
 
-
-
 // A very clean way to pass around large LISTS (arrays) of COLLECTIONS (objects)
 // of Data is to have an Array full of objects. 
 
 //Create an empty array called users.
 
   //Code Here
+var users = [];
 
 // Now add three user objects to your users array. Each user object should contain the
 // following properties. name, email, password, username.
@@ -306,16 +311,36 @@ var user1 = {
 }
 
 //Your Code Here
+var user2 = {
+    name: 'Jake Lingwall',
+    email: 'jake.lingwall@gmail.com',
+    password: 'iLoveToCode',
+    username: 'jlingwall'
+}
 
+var user3 = {
+    name: 'Michael Frampton',
+    email: 'mikef521@gmail.com',
+    password: 'iLoveToCreate',
+    username: 'mikef'
+}
+users.push(user1);
+users.push(user2);
+users.push(user3);
 // Now you have a very common data structure. Twitter is a good use case.
 // It's easy to imagine that your followers list on Twitter is an Array full or objects
-// and those objects contain properties about the specific person you follow.*/
+// and those objects contain properties about the specific person you follow.
 
 // Now let's say that Tyler decided to delete his account. Loop through your array of
 // objects until you find Tyler's account (use tylermcginnis33@gmail.com to find him).
 // Once you find the particular indice he's located in, delete him from the array.
 
   //Code Here
+for (var i = 0; i < users.length; i++) {
+  if (users[i].email === "tylermcginnis33@gmail.com") {
+    users.splice(i, 1);
+  }
+}
 
 // The activity we just did is very much how data works in 'the real world'.
 
